d954bf2aebebf094d8186ab7b20056fc39bd98ba
junna-oikawa <jnn.y326@gmail.com>
2021-05-25 13:28:31 +0000

    [Finish] 3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,18 @@ class BooksController < ApplicationController
   end
 
   def index
-    @book = Book.new
     @books = Book.all
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      redirect_to book_path(@book.id), notice:'Book was successfully created.'
+    else
+      render :index
+    end
   end
 
   def show
@@ -23,14 +27,19 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      redirect_to book_path(book.id), notice:'Book was successfully updated.'
+    else
+      @book = book
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      redirect_to books_path, notice:'Book was successfully destroyed.'
+    end
   end
 
 
